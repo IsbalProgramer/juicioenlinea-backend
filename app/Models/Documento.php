485145d4029b8886f198a910b4617b4032459c23
Mildred20Ricardez <20161303@itoaxaca.edu.mx>
2025-03-23 22:50:39 -0600

    documemto

--- app/Models/Documento.php
+++ app/Models/Documento.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Documento extends Model
 {
-    //
+    //Clase documento
+    protected $table = 'documentos';
+    
+    protected $fillable = [
+        'idExpediente',
+        'folioPreregistro',
+        'nombre',
+        'documento'
+    ];
+    
 }
